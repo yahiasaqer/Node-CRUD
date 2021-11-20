12d1aa25b6ef328a4068282a7faa9ca68cc7daa6
yahiasaqer <yahiasaqer@gmail.com>
2021-11-20 08:19:18 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express=require('express');
 const bodyParser=require('body-parser');
 const api = require('./api');
   
-const port=3000;
+const port= process.env.PORT || 3000;
 const app=express();
    
 app.listen(port, function() {
